79338a319bf3e39f44747718164834ae9f32a796
Nathan Louth <77612558+NathanLouth@users.noreply.github.com>
2025-02-28 13:42:55 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# 1. Comment out lines in /etc/apt/sources.list that include "cdrom:"
+sed -i '/cdrom:/s/^/#/' /etc/apt/sources.list
+
+# 2. Update the system
+apt update -y
+
+# 3. Install necessary packages
+apt install -y xorg xinit chromium
+
+# 4. Create the directory for the systemd service override
+mkdir -p /etc/systemd/system/getty@tty1.service.d/
+
+# 5. Create the override.conf file
+cat > /etc/systemd/system/getty@tty1.service.d/override.conf <<EOL
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin kiosk --noclear %I \$TERM
+EOL
+
+# 6. Add "startx" to the end of /home/kiosk/.bashrc
+echo "startx" >> /home/kiosk/.bashrc
+
+# 7. Create the .xinitrc file for the kiosk user
+cat > /home/kiosk/.xinitrc <<EOL
+#!/bin/bash
+
+sleep 3
+
+xrandr --output Virtual-1 --mode 1920x1080
+
+sleep 2
+
+SCREEN_RESOLUTION=\$(xrandr | grep '*' | awk '{print \$1}')
+
+WIDTH=\$(echo \$SCREEN_RESOLUTION | cut -d 'x' -f 1)
+HEIGHT=\$(echo \$SCREEN_RESOLUTION | cut -d 'x' -f 2)
+
+chromium --no-sandbox --kiosk --window-position=0,0 --window-size=\$WIDTH,\$HEIGHT "https://www.bbc.co.uk/news"
+EOL
+
+# 8. Make .xinitrc owned by the kiosk user and executable
+chown kiosk:kiosk /home/kiosk/.xinitrc
+chmod +x /home/kiosk/.xinitrc
+
+# 9. Reboot the system
+reboot
