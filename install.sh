1150aac46c2fc0b0c7ee07c6761715de7f199bce
Nathan Louth <77612558+NathanLouth@users.noreply.github.com>
2025-03-03 17:38:42 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ ExecStart=-/sbin/agetty --autologin kiosk --noclear %I \$TERM
 EOL
 
 # 6. Add "startx" to the end of /home/kiosk/.bashrc if it isn't already
+grep -qxF "startx" /home/kiosk/.bashrc || echo "sleep 5" >> /home/kiosk/.bashrc
 grep -qxF "startx" /home/kiosk/.bashrc || echo "startx" >> /home/kiosk/.bashrc
 
 # 7. Create the .xinitrc file for the kiosk user
