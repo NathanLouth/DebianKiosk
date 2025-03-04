933d13c1ea7dac0e42036a1d5abf52b16367cea0
Nathan Louth <77612558+NathanLouth@users.noreply.github.com>
2025-03-04 20:48:25 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -123,7 +123,7 @@ SCREEN_RESOLUTION=\$(xrandr | grep '*' | awk '{print \$1}')
 WIDTH=\$(echo \$SCREEN_RESOLUTION | cut -d 'x' -f 1)
 HEIGHT=\$(echo \$SCREEN_RESOLUTION | cut -d 'x' -f 2)
 
-$BROWSER --kiosk --window-position=0,0 --window-size=\$WIDTH,\$HEIGHT URL
+$BROWSER --kiosk --window-position=0,0 --window-size=\$WIDTH,\$HEIGHT "$URL"
 
 while pgrep -x "$BROWSER" > /dev/null; do
     sleep 10
